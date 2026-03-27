84d36b32344b555eb52db9b9d98100ca3b0466ef
Mark Mroz <markmroz95@gmail.com>
2026-03-27 09:59:54 -0400

    use 0.2.3

--- lib/flinks/resources/resource.rb
+++ lib/flinks/resources/resource.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'active_support/inflector'
 require 'pp'
 
 module Flinks
@@ -30,6 +31,14 @@ module Flinks
         key.to_s
       end
 
+      def as_json(*)
+        deep_snake_case(attributes)
+      end
+
+      def to_json(*args)
+        as_json.to_json(*args)
+      end
+
       def inspect
         "#<#{self.class.name} #{formatted_attributes}>"
       end
@@ -42,6 +51,21 @@ module Flinks
 
       def formatted_attributes
         attributes.map { |key, value| "#{key}=#{value.inspect}" }.join(', ')
+      end
+
+      def deep_snake_case(value)
+        case value
+        when Resource
+          value.as_json
+        when Array
+          value.map { |item| deep_snake_case(item) }
+        when Hash
+          value.each_with_object({}) do |(key, nested_value), result|
+            result[ActiveSupport::Inflector.underscore(key.to_s)] = deep_snake_case(nested_value)
+          end
+        else
+          value
+        end
       end
     end
   end
